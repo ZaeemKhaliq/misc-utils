abd7a51a52a808fcc74f5a4cc89a07710e732475
Zaeem Khaliq <khaliqzaeem@hotmail.com>
2023-12-14 16:37:45 +0500

    chore(main): CDatePicker updates

--- components/tsx/CDatePicker.tsx
+++ components/tsx/CDatePicker.tsx
@@ -3,10 +3,20 @@ import { useEffect, useState } from "react";
 import {
   CalendarTodayOutlined,
   Close,
+  KeyboardArrowDown,
   KeyboardArrowLeft,
   KeyboardArrowRight,
+  KeyboardArrowUp,
 } from "@mui/icons-material";
-import { Box, Dropdown, Menu, MenuButton, Typography, styled } from "@mui/joy";
+import {
+  Box,
+  Dropdown,
+  IconButton,
+  Menu,
+  MenuButton,
+  Typography,
+  styled,
+} from "@mui/joy";
 
 const months = [
   "Jan",
@@ -36,6 +46,11 @@ const datesValuesMatrixInitState = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
 ];
+const yearsSelectionMatrixInitState = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
 
 type CDatePickerProps = {
   value?: Date;
@@ -64,78 +79,256 @@ export default function CDatePicker(props: CDatePickerProps) {
     setMonth(value.getMonth() + 1);
   }, [value]);
 
-  const [datesFlagsMatrix, setDatesFlagsMatrix] = useState<number[][]>(
-    datesFlagsMatrixInitState
+  const [yearsSelectionMatrix, setYearsSelectionMatrix] = useState<number[][]>(
+    yearsSelectionMatrixInitState
   );
-  const [dateValuesMatrix, setDateValuesMatrix] = useState<number[][]>(
-    datesValuesMatrixInitState
-  );
+  const [currentYearVector, setCurrentYearVector] = useState<number[]>([]);
   const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false);
-  console.log({ datesFlagsMatrix, dateValuesMatrix });
+  const [isYearSelectorOpen, setIsYearSelectorOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    setDatesFlagsMatrix(prev => {
-      let daysAdded = 0;
-      let datesAfterLastDateOfCurrentMonth = 1;
-      const dateValuesMatrixLocal = structuredClone(dateValuesMatrix);
+  const datesFlagsMatrix: number[][] = (() => {
+    let daysAdded = 0;
+    const prev = structuredClone(datesFlagsMatrixInitState);
 
-      for (let i = 0; i < prev.length; i += 1) {
-        const row = prev[i];
+    for (let i = 0; i < prev.length; i += 1) {
+      const row = prev[i];
 
-        for (let j = 0; j < row.length; j += 1) {
-          const sum = i + j + 1;
-          if (sum === startDayOfMonth && i === 0) {
-            const previousMonth = month - 1;
-            let previousMonthLastDate = new Date(
-              year,
-              previousMonth,
-              0
-            ).getDate();
-
-            for (let k = sum - 2; k >= 0; k--) {
-              dateValuesMatrixLocal[i][k] = previousMonthLastDate;
-              prev[i][k] = 0;
-              previousMonthLastDate -= 1;
-            }
+      for (let j = 0; j < row.length; j += 1) {
+        const sum = i + j + 1;
+        if (sum === startDayOfMonth && i === 0) {
+          for (let k = sum - 2; k >= 0; k--) {
+            prev[i][k] = 0;
           }
+        }
 
-          if (
-            (sum >= startDayOfMonth || i > 0) &&
-            daysAdded <= daysInCurrentMonth
-          ) {
-            prev[i][j] = 1;
-            daysAdded += 1;
-            if (daysAdded === currentDate) {
-              prev[i][j] = 2;
-            }
-            dateValuesMatrixLocal[i][j] = daysAdded;
+        if (
+          (sum >= startDayOfMonth || i > 0) &&
+          daysAdded <= daysInCurrentMonth
+        ) {
+          prev[i][j] = 1;
+          daysAdded += 1;
+          if (daysAdded === currentDate) {
+            prev[i][j] = 2;
           }
+        }
 
-          if (daysAdded > daysInCurrentMonth) {
-            dateValuesMatrixLocal[i][j] = datesAfterLastDateOfCurrentMonth;
-            datesAfterLastDateOfCurrentMonth += 1;
-            prev[i][j] = 0;
+        if (daysAdded > daysInCurrentMonth) {
+          prev[i][j] = 0;
+        }
+      }
+    }
+
+    return prev;
+  })();
+  const dateValuesMatrix: number[][] = (() => {
+    let daysAdded = 0;
+    let datesAfterLastDateOfCurrentMonth = 1;
+    const dateValuesMatrixLocal = structuredClone(datesValuesMatrixInitState);
+
+    for (let i = 0; i < dateValuesMatrixLocal.length; i += 1) {
+      const row = dateValuesMatrixLocal[i];
+
+      for (let j = 0; j < row.length; j += 1) {
+        const sum = i + j + 1;
+
+        if (sum === startDayOfMonth && i === 0) {
+          const previousMonth = month - 1;
+          let previousMonthLastDate = new Date(
+            year,
+            previousMonth,
+            0
+          ).getDate();
+          for (let k = sum - 2; k >= 0; k--) {
+            dateValuesMatrixLocal[i][k] = previousMonthLastDate;
+            previousMonthLastDate -= 1;
           }
+        }
+
+        if (
+          (sum >= startDayOfMonth || i > 0) &&
+          daysAdded <= daysInCurrentMonth
+        ) {
+          daysAdded += 1;
+          dateValuesMatrixLocal[i][j] = daysAdded;
+        }
+
+        if (daysAdded > daysInCurrentMonth) {
+          dateValuesMatrixLocal[i][j] = datesAfterLastDateOfCurrentMonth;
+          datesAfterLastDateOfCurrentMonth += 1;
+        }
+      }
+    }
+    return dateValuesMatrixLocal;
+  })();
+  console.log({
+    datesFlagsMatrix,
+    dateValuesMatrix,
+    yearsSelectionMatrix,
+    currentYearVector,
+  });
+
+  const computeYearsSelectionMatrixFromVector = (yearsArr: number[]) => {
+    const yearsGrid = [yearsArr];
+
+    for (let i = 0; i <= 1; i++) {
+      const newYearsArr = [];
+      for (let j = 1; j <= yearsArr.length; j++) {
+        if (i === 0) {
+          newYearsArr.push(yearsArr[0] - j);
+        }
+        if (i === 1) {
+          newYearsArr.push(yearsArr[yearsArr.length - 1] + j);
         }
       }
 
-      setDateValuesMatrix(dateValuesMatrixLocal);
+      if (i === 0) {
+        newYearsArr.reverse();
+        yearsGrid.unshift(newYearsArr);
+      }
+      if (i === 1) {
+        yearsGrid.push(newYearsArr);
+      }
+    }
 
-      return prev;
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentDate, daysInCurrentMonth, month, startDayOfMonth, year]);
+    setYearsSelectionMatrix(yearsGrid);
+  };
 
-  const [isYearSelectorOpen, setIsYearSelectorOpen] = useState<boolean>(false);
+  const yearsSelectionMatrixRowLength = yearsSelectionMatrix[0].length;
+  useEffect(() => {
+    function leapYear(yearToCheck: number) {
+      return (
+        (yearToCheck % 4 == 0 && yearToCheck % 100 != 0) ||
+        yearToCheck % 400 == 0
+      );
+    }
+
+    const yearsArr = [];
+    yearsArr.push(year);
+
+    if (leapYear(year)) {
+      const yearsArrLength = yearsArr.length;
+      for (
+        let i = 1;
+        i <= yearsSelectionMatrixRowLength - yearsArrLength;
+        i += 1
+      ) {
+        yearsArr.unshift(year + i);
+      }
+    } else {
+      let previousYear = year - 1;
+      yearsArr.push(previousYear);
+      if (leapYear(previousYear)) {
+        const yearsArrLength = yearsArr.length;
+
+        for (
+          let i = 1;
+          i <= yearsSelectionMatrixRowLength - yearsArrLength;
+          i += 1
+        ) {
+          yearsArr.unshift(year + i);
+        }
+      } else {
+        previousYear = year - 2;
+        yearsArr.push(previousYear);
+
+        if (leapYear(previousYear)) {
+          const yearsArrLength = yearsArr.length;
+
+          for (
+            let i = 1;
+            i <= yearsSelectionMatrixRowLength - yearsArrLength;
+            i += 1
+          ) {
+            yearsArr.unshift(year + i);
+          }
+        } else {
+          previousYear = year - 3;
+          yearsArr.push(previousYear);
+        }
+      }
+    }
+    yearsArr.reverse();
+
+    setCurrentYearVector(yearsArr);
+    computeYearsSelectionMatrixFromVector(yearsArr);
+  }, [year, yearsSelectionMatrixRowLength]);
+
+  const yearsSelectionMatrixAllElemsLength = yearsSelectionMatrix.flat().length;
+  const computeNextYearsSelectionMatrix = (type: "inc" | "dec") => {
+    const cloned = structuredClone(yearsSelectionMatrix);
+
+    for (let rowIndex = 0; rowIndex < cloned.length; rowIndex += 1) {
+      const row = cloned[rowIndex];
+
+      for (let colIndex = 0; colIndex < row.length; colIndex += 1) {
+        if (type === "inc") {
+          cloned[rowIndex][colIndex] += yearsSelectionMatrixAllElemsLength;
+        } else {
+          cloned[rowIndex][colIndex] -= yearsSelectionMatrixAllElemsLength;
+        }
+      }
+    }
+
+    setYearsSelectionMatrix(cloned);
+  };
+
+  //   useEffect(() => {
+  // setDatesFlagsMatrix(prev => {
+  //   let daysAdded = 0;
+  //   let datesAfterLastDateOfCurrentMonth = 1;
+  //   const dateValuesMatrixLocal = structuredClone(dateValuesMatrix);
+  //   for (let i = 0; i < prev.length; i += 1) {
+  //     const row = prev[i];
+  //     for (let j = 0; j < row.length; j += 1) {
+  //       const sum = i + j + 1;
+  //       if (sum === startDayOfMonth && i === 0) {
+  //         const previousMonth = month - 1;
+  //         let previousMonthLastDate = new Date(
+  //           year,
+  //           previousMonth,
+  //           0
+  //         ).getDate();
+  //         for (let k = sum - 2; k >= 0; k--) {
+  //           dateValuesMatrixLocal[i][k] = previousMonthLastDate;
+  //           prev[i][k] = 0;
+  //           previousMonthLastDate -= 1;
+  //         }
+  //       }
+  //       if (
+  //         (sum >= startDayOfMonth || i > 0) &&
+  //         daysAdded <= daysInCurrentMonth
+  //       ) {
+  //         prev[i][j] = 1;
+  //         daysAdded += 1;
+  //         if (daysAdded === currentDate) {
+  //           prev[i][j] = 2;
+  //         }
+  //         dateValuesMatrixLocal[i][j] = daysAdded;
+  //       }
+  //       if (daysAdded > daysInCurrentMonth) {
+  //         dateValuesMatrixLocal[i][j] = datesAfterLastDateOfCurrentMonth;
+  //         datesAfterLastDateOfCurrentMonth += 1;
+  //         prev[i][j] = 0;
+  //       }
+  //     }
+  //   }
+  //   setDateValuesMatrix(dateValuesMatrixLocal);
+  //   return prev;
+  // });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   }, [currentDate, daysInCurrentMonth, month, startDayOfMonth, year]);
+
   const onYearMonthClick = () => {
     setIsYearSelectorOpen(true);
+
+    computeYearsSelectionMatrixFromVector(currentYearVector);
   };
 
   const onPreviousClick = () => {
     const newMonthValue = month - 1;
     if (newMonthValue === 0) {
       setMonth(12);
-      setYear(prev => prev - 1);
+      setYear((prev) => prev - 1);
       return;
     }
 
@@ -145,7 +338,7 @@ export default function CDatePicker(props: CDatePickerProps) {
     const newMonthValue = month + 1;
     if (newMonthValue === 13) {
       setMonth(1);
-      setYear(prev => prev + 1);
+      setYear((prev) => prev + 1);
       return;
     }
 
@@ -158,7 +351,7 @@ export default function CDatePicker(props: CDatePickerProps) {
         <MenuButton
           sx={{ gap: "0.25rem" }}
           onClick={() =>
-            setIsCalendarOpen(prev => {
+            setIsCalendarOpen((prev) => {
               const newValue = !prev;
               if (newValue === false) {
                 setYear(initYear);
@@ -182,23 +375,70 @@ export default function CDatePicker(props: CDatePickerProps) {
           {isYearSelectorOpen && (
             <Box className="year-selector">
               <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                <Close onClick={() => setIsYearSelectorOpen(false)} />
+                <IconButton>
+                  <Close
+                    onClick={() => setIsYearSelectorOpen(false)}
+                    sx={{ cursor: "pointer", fontSize: "1.5rem" }}
+                  />
+                </IconButton>
               </Box>
-              <Box className="year-selector-grid">abcd</Box>
+              <Box className="year-selector-grid-container">
+                <IconButton>
+                  <KeyboardArrowUp
+                    sx={{ fontSize: "2.5rem", cursor: "pointer" }}
+                    onClick={() => {
+                      computeNextYearsSelectionMatrix("dec");
+                    }}
+                  />
+                </IconButton>
+                <Box className="year-selector-grid">
+                  {yearsSelectionMatrix.map((row) => {
+                    return row.map((yr) => {
+                      return (
+                        <Typography
+                          key={yr}
+                          className={`year-selector-year ${
+                            yr === year ? "year-selector-year--selected" : ""
+                          }`}
+                          onClick={() => {
+                            setYear(yr);
+                            setIsYearSelectorOpen(false);
+                          }}
+                        >
+                          {yr}
+                        </Typography>
+                      );
+                    });
+                  })}
+                </Box>
+                <IconButton>
+                  <KeyboardArrowDown
+                    sx={{ fontSize: "2.5rem", cursor: "pointer" }}
+                    onClick={() => {
+                      computeNextYearsSelectionMatrix("inc");
+                    }}
+                  />
+                </IconButton>
+              </Box>
             </Box>
           )}
 
           <Box className="picker-header">
-            <Typography className="selected-date">
+            <Typography className="selected-date" onClick={onYearMonthClick}>
               {months[month - 1]} {year}
+              <KeyboardArrowRight sx={{ fontSize: "2rem" }} />
             </Typography>
 
             <Box className="move-actions-container">
               <Typography className="action-previous" onClick={onPreviousClick}>
-                <KeyboardArrowLeft sx={{ fontSize: "1.5rem" }} />
+                <IconButton>
+                  <KeyboardArrowLeft sx={{ fontSize: "1.5rem" }} />
+                </IconButton>
               </Typography>
               <Typography className="action-next" onClick={onNextClick}>
-                <KeyboardArrowRight sx={{ fontSize: "1.5rem" }} />
+                <IconButton>
+                  <KeyboardArrowRight sx={{ fontSize: "1.5rem" }} />
+                </IconButton>
               </Typography>
             </Box>
           </Box>
@@ -258,6 +498,8 @@ export default function CDatePicker(props: CDatePickerProps) {
 const CDatePickerStyled = styled(Box)(() => ({
   width: "max-content",
   position: "relative",
+  display: "flex",
+  alignItems: "center",
 
   ".selector-box": {
     borderRadius: "5px",
@@ -277,6 +519,9 @@ const MuiMenuStyled = styled(Menu)(() => ({
     fontSize: "1.25rem",
     fontWeight: 700,
     color: "#3c3c3c",
+    cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
   },
 
   ".picker-container": {
@@ -349,8 +594,44 @@ const MuiMenuStyled = styled(Menu)(() => ({
     height: "100%",
     width: "100%",
     zIndex: 1,
-    padding: "28px",
+    padding: "12px",
     backgroundColor: "#ffffff",
     borderRadius: "8px",
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  ".year-selector-grid-container": {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+
+  ".year-selector-grid": {
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(4,1fr)",
+    rowGap: "1rem",
+    columnGap: "0.25rem",
+    placeItems: "center",
+  },
+
+  ".year-selector-year": {
+    textAlign: "center",
+    borderRadius: "100%",
+    cursor: "pointer",
+    width: "max-content",
+    padding: "16px",
+
+    "&:hover": {
+      backgroundColor: "#90CFB6",
+    },
+  },
+
+  ".year-selector-year--selected": {
+    backgroundColor: "#90CFB6",
   },
 }));
